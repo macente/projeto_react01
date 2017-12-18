0a8d051a21f036634c76a9cb4a521a7d4bf725b7
macente <anderson.macente@gmail.com>
2017-12-18 21:09:04 -0200

    lista json - components

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+import AppTitle from './src/components/app-title';
+
+const App = () => {
+    return(
+        <View>
+          <AppTitle texto={'Próximos Lançamentos'} />
+        </View>
+    );
+}
 
 AppRegistry.registerComponent('projeto_react01', () => App);
---
+++ src/components/app-title.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AppTitle = (props) => {
+    const { estiloText, estiloView } = estilos;
+    return(
+      <View style={estiloView}>
+        <Text style={estiloText}>{props.texto}</Text>
+      </View>
+    );
+}
+
+const estilos = {
+    estiloView: {
+        backgroundColor: '#3C4E5C',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 10,
+        position: 'relative'
+
+    },
+    estiloText: {
+        fontSize: 20,
+        color:'#FFFFFF'
+    }
+};
+
+export default AppTitle;
